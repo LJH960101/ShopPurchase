13a74560d144437adfd61c7e656f7438e74ff4a2
ljh960101 <ljh960101@gmail.com>
2026-09-01 15:37:31 +0900

    Wire up JHTimingWheel.Stop() at shutdown
    Was declared but never called anywhere. Called at the end of Main after
    all demo tests run, as the graceful-shutdown path a real server would
    need for this component even though the tick thread is background and
    wouldn't itself block process exit.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using ShopPurchase.Core.Thread;
 using ShopPurchase.Test;
 
 namespace ShopPurchase
@@ -11,6 +12,11 @@ namespace ShopPurchase
             BulkGrantTest.Run();
             MultiKeyScheduleTest.Run();
             JHSerializedObjectTest.Run();
+
+            // 데모가 다 끝났으니 tick 스레드를 명시적으로 정리한다. tick 스레드는 IsBackground=true라
+            // Stop()을 안 불러도 프로세스 종료 자체는 막지 않지만, 실제 서버라면 다른 서비스를 계속
+            // 띄워둔 채로 이 컴포넌트만 골라서 내려야 할 수 있으니 그런 정상 종료 경로를 갖춰둔다.
+            JHTimingWheel.Instance.Stop();
         }
     }
 }
